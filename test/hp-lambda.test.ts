9645830a54e8036bfba63ca8280b59758d24a6d1
Piyush Dubey <piyush.dubey@hp.com>
2024-09-17 09:22:18 +0530

    Initial commit

---
+++ test/hp-lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HpLambda from '../lib/hp-lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hp-lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HpLambda.HpLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
